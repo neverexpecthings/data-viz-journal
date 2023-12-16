449bb0882941d6641cd4b18022ba55224b57cfac
nver <77342876+neverexpecthings@users.noreply.github.com>
2023-12-16 16:01:41 -0400

    new random_walk exercise

--- Day0/cubes.py
+++ Day0/cubes.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
+plt.style.use("Solarize_Light2")
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.scatter(x_values, y_values, c=y_values, cmap=cm.Blues, s=10)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,4 +16,5 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set siza of tick labels.
 ax.tick_params(labelsize=14)
 
-plt.show()
+plt.savefig("colored_cubes5000.png", bbox_inches="tight")
+# plt.show()
--- Day0/mpl_squares.py
+++ Day0/mpl_squares.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use("Solarize_Light2")
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+ax.scatter(2, 4, s=200)
 
 # set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
