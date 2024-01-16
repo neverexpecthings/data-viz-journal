323465589dfcab82d05aab8d3bd77c51febc5f60
neverexpecthings <neverexpecthings@gmail.com>
2024-01-16 15:53:38 -0400

    update

--- Day2/dice_visual.py
+++ Day2/dice_visual.py
@@ -7,18 +7,12 @@ die_1 = Die()
 die_2 = Die()
 
 # Let's make some rolls, and store results in a list.
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_numb in range(5000)]
 
 # Analize the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(2, max_result + 1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
 
 # Let's visualize the results.
 title = "Results of Rolling Two D6 Dice 1,000 Times."
